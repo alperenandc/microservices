562ae3a5120658f563bb4553dd46c13ecbce7745
alperenandc <andicalperen@gmail.com>
2026-04-04 13:17:08 +0300

    Revert "Merge pull request #6 from alperenandc/dev-eren"
    This reverts commit 86fc9f3c5e7819be33d331e8abed726633601efa, reversing
    changes made to 6ca15dffedf68ea5aebd72f22cff443d3600cd4d.

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-// ÜRÜN LİSTESİ 
-app.get('/api/products', (req, res) => {
-    res.json([{ id: 101, name: "Klavye" }, { id: 102, name: "Mouse" }]);
-});
-
-app.listen(3002, () => console.log("Product odası 3002 portunda hazır!"));
